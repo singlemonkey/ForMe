fa9ace51a702d2b1bf664e34a1032a0fab374fb3
zhangqi <903012206@qq.com>
2016-08-17 17:41:02 +0800

    完善HomeController

--- MyLife/Controllers/HomeController.cs
+++ MyLife/Controllers/HomeController.cs
@@ -8,6 +8,23 @@ namespace MyLife.Controllers
 {
     public class HomeController : Controller
     {
-       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
