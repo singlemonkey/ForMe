3da13af1b1d94c23e8f112cb9e2b22b2065ce35f
zhangqi <903012206@qq.com>
2016-08-18 18:27:28 +0800

    更新

---
+++ MyLife/Context/MyLifeInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MyLife.Models;
+
+namespace MyLife.Context
+{
+    public class MyLifeInitializer:System.Data.Entity.DropCreateDatabaseIfModelChanges<MyLifeContext>
+    {
+        protected override void Seed(MyLifeContext context)
+        {
+            var administrators = new List<AdminModel> {
+                new AdminModel {
+                    Account="903012206",
+                    Password="missyou?",
+                    Name="日光倾城",
+                    text="text"
+                }
+            };
+            administrators.ForEach(a => context.Administrators.Add(a));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MyLife/Controllers/AdminController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyLife.Context;
+using MyLife.Models;
+
+namespace MyLife.Controllers
+{
+    public class AdminController : Controller
+    {
+        private MyLifeContext db = new MyLifeContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.Administrators.ToList());
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminModel adminModel = db.Administrators.Find(id);
+            if (adminModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adminModel);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Account,Password")] AdminModel adminModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Administrators.Add(adminModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(adminModel);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminModel adminModel = db.Administrators.Find(id);
+            if (adminModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adminModel);
+        }
+
+        // POST: Admin/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Account,Password")] AdminModel adminModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(adminModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(adminModel);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminModel adminModel = db.Administrators.Find(id);
+            if (adminModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adminModel);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AdminModel adminModel = db.Administrators.Find(id);
+            db.Administrators.Remove(adminModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
