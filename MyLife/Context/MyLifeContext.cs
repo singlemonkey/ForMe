3555984a9c880c8ed092651c65062cd1b7637531
zhangqi <903012206@qq.com>
2016-08-18 18:29:00 +0800

    更新

---
+++ MyLife/Context/MyLifeContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MyLife.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MyLife.Context
+{
+    public class MyLifeContext:DbContext
+    {
+        public MyLifeContext() : base("MyLifeContext") {
+
+        }
+        public DbSet<AdminModel> Administrators { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
