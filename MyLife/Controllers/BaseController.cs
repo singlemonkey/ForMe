afc10c6aa58d13530777874225c44a6284bf0f69
zhangqi <903012206@qq.com>
2016-08-19 17:55:17 +0800

    错误日志

---
+++ MyLife/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyLife.Controllers
+{
+    public class BaseController: Controller
+    {
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            //获取异常信息
+            Exception Error = filterContext.Exception;
+            logger.Error(Error);
+        }
+    }
+}
--- MyLife/Global.asax.cs
+++ MyLife/Global.asax.cs
@@ -6,16 +6,20 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+using System.IO;
+
 namespace MyLife
-{
+{    
     public class MvcApplication : System.Web.HttpApplication
     {
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(Server.MapPath("~/Web.config")));
         }
     }
 }
