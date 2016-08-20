0df17a13b9642933bfd75b7f08d521804787aa27
zhangqi <903012206@qq.com>
2016-08-20 17:11:55 +0800

    js

---
+++ MyLife/Frontend/js/app/index.js
@@ -0,0 +1,52 @@
+﻿class Sidebar {
+    constructor(menus) {
+        this.menus = menus;
+    }
+    render() {
+        this.menus.map((e) => {
+            alert(e.title);
+        });
+    }
+};
+jQuery(document).ready(function () {
+    //左侧菜单项设置
+    let menuItems = [
+        {
+            title: "生活动态",
+            modules: [
+                {
+                    title: "说说",
+                    url: "",
+                    icon:"",
+                },
+                {
+                    title: "博客",
+                    url: "",
+                    icon: "",
+                },
+                {
+                    title: "设置",
+                    url: "",
+                    icon: "",
+                },
+            ]
+        },
+        {
+            title: "资产管理",
+            modules: [
+                {
+                    title: "购物车",
+                    url: "",
+                    icon: "",
+                },
+                {
+                    title: "资债统计",
+                    url: "",
+                    icon: "",
+                },
+            ]
+        }
+    ];
+    let a = new Sidebar(menuItems);
+    a.render();
+});
