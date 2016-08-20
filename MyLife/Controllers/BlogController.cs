144c5f17bdfa92caa1c500f1780063e16a822fcd
zhangqi <903012206@qq.com>
2016-08-20 17:11:55 +0800

    js

---
+++ MyLife/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyLife.Context;
+using MyLife.Models;
+
+namespace MyLife.Controllers
+{
+    public class BlogController:Controller
+    {
+        private MyLifeContext db = new MyLifeContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
