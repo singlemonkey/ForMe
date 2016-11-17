9b53fb033403c46121e30568c09415d864a82bc5
zhangqi <903012206@qq.com>
2016-11-17 15:12:36 +0800

    更新字典管理

--- MyLife/Frontend/js/app/dictonary.js
+++ MyLife/Frontend/js/app/dictonary.js
@@ -1,24 +1,85 @@
-﻿class WaterFall {
+﻿class Dictionary {
     constructor(props) {
         let defaultOption = {
-            'columnWieth': 200,
+            'defaultWidth': 200,
         }
         this.container = $(props.container);        
         this.options = Object.assign(defaultOption, props);
+        this.items = 1;
         this.init();
     }
-
+    //初始化各项参数
     init() {
-        for (var i = 0,l=this.options.data.length; i < l; i++) {
+        let width = this.container.width();
+        this.column = Math.round(width / this.options.defaultWidth);
+        let getColumns = () => {
+            var arr = [];
+            for (var i = 0, l = this.column; i < l; i++) {
+                arr.push(0);
+            }
+            return arr;
+        }
+        this.columns = getColumns();
+        this.columnWidth = width / this.column;
+        this.render();
+    }
+
+    render() {
+        let self = this;
+        self.renderColumn();
+        for (var i = 0, l = this.options.data.length; i < l; i++) {
             this.renderItem(this.options.data[i]);
         }
     }
-
+    //先渲染布局列
+    renderColumn() {
+        let self=this;
+        for (var i = 0; i < self.column; i++) {
+            let column = $("<div></div>", {
+                'class': 'column column' + i,
+                style: 'width:' + (self.columnWidth-40)+'px',
+            })
+            let columnUl = $("<ul></ul>", {
+                id:'columnUl'+i,
+            });
+            column.append(columnUl);
+            self.container.append(column);
+        }
+    }
+    //渲染各个字典项
     renderItem(dictonary) {
-        let container = this.container;
+        let self=this;
+        let itemContainer;
+        if (self.items <=self.column){
+            itemContainer = $(".column" + (self.items-1) + " ul");
+        } else {
+            let indexOfSamllest = () =>{
+                let lowest = 0;
+                const columns=self.columns;
+                for (var i = 0; i < columns.length; i++) {
+                    if(columns[i]<columns[lowest]) lowest=i;
+                }
+                return lowest;
+            }
+            itemContainer = $(".column" + indexOfSamllest() + " ul");
+        }
         let tmpl = $.templates("#dictonariesUnit");
         let unit = tmpl.render(dictonary);
-        container.append(unit);
+        itemContainer.append(unit);
+        //更新items(已加载的字典数)和各列的高度
+        self.update(self.items-1);        
     }
-
+    
+    update(index) {
+        this.items += 1;
+        let height = $(".column" + index).height();
+        this.columns[index] = height;
+    }
 };
+
+jQuery(document).ready(function () {
+    let Dictionary = new Dictionary({
+        'data': dictorys,
+        'container': '.container'
+    });
+});
